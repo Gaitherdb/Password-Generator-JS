92ae51c8899777b30872e1960034698ff8ad065b
David Gaither <gaitherdb@gmail.com>
2021-06-11 02:28:39 -0400

    complete random gen with selected characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,50 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerChars = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
+
 const symbols = "!@#$%^&*_-+=";
-var allChar = alpha + lowerChars + numbers + symbols;
+const numbers = "0123456789";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-function generatePassword(){
-var numchar = window.prompt("How many characters would you like your password to contain?");
 
-if (!numchar) {
-  return;
-}
+function generatePassword() {
+  var numChar= window.prompt("How many characters would you like your password to contain?");
 
-if (numchar < 8 || numchar > 128) {
-  window.alert("Password length must be at least 8 characters");
-  return;
-}
+  if (!numChar) {
+    return;
+  }
 
-var conSymb = window.confirm("Press OK to confirm including special characters");
-var conNum = window.confirm("Press OK to confirm including numeric characters");
-var conUpper = window.confirm("Press OK to confirm including uppercase letters");
-var conLower = window.confirm("Press OK to confirm including lowercase letters");
+  if (numChar < 8 || numChar > 128) {
+    window.alert("Password length must be at least 8 characters");
+    return;
+  }
 
-if (!conSymb && !conNum && !conUpper && !conLower){
-  window.alert("You must select at least one character type");
-}
+  var conSymb = window.confirm("Press OK to confirm including special characters");
+  var conNum = window.confirm("Press OK to confirm including numeric characters");
+  var conUpper = window.confirm("Press OK to confirm including uppercase letters");
+  var conLower = window.confirm("Press OK to confirm including lowercase letters");
+
+  if (!conSymb && !conNum && !conUpper && !conLower) {
+    window.alert("You must select at least one character type");
+    return;
+  }
+
+  var temp = '';
+  var newPW = '';
+
+  if (conSymb) { temp += symbols };
+  if (conNum) { temp += numbers };
+  if (conUpper) { temp += upperCase };
+  if (conLower) { temp += lowerCase };
+
+
+  for (var i = 0; i < numChar; i++) {
+    newPW += temp[Math.floor(Math.random() * temp.length)];
+  }
+  return newPW;
+  // var index = Math.floor(Math.random() * options.length);
+  // var computerChoice = options[index];
+  // Check if questions are true,  add all valid strings together, randomize them, .join (" ")
 }
 
 
