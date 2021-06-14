543e3317be233bfb298adcd1ab96559b08cc436a
David Gaither <gaitherdb@gmail.com>
2021-06-14 17:50:26 -0400

    Final commit- add comments and README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Password Generator
+
+This is a password generator using Javascript, HTML & CSS. 
+
+## Functionality
+
+This webpage generates a random password based on the critiera selected by the user. 
+
+The password must be 8-128 characters long, as decided by the user via a prompt. 
+
+The application confirms whether the user wants to use:
+* Uppercase letters
+* Lowercase letters
+* Numbers
+* Special characters
+
+When the button titled Generate Password is clicked, the first prompted is activated.
+
+The password is randomized and displayed in the text area box after the user selects their desired criteria. 
+
+## Additional Info
+
+![image](./Assets/passwordgenerator.png)
+
+Deployed [Password Generator](https://gaitherdb.github.io/Password-Generator-JS/) site on Github.
+
+This application is made only using what we learned in class. I made use of the prompt, alert, & confirm attributes along with If statements and For loops.
+
+**Version 1.0**
--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
 function generatePassword() {
   var numChar= window.prompt("How many characters would you like your password to contain?");
-
+// if the user selects cancel, the app will end.
   if (!numChar) {
     return;
   }
-
+// if the user chooses to have a password lower than 8 or greater than 128 characters, the app will end.
   if (numChar < 8 || numChar > 128) {
     window.alert("Password length must be at least 8 characters");
     return;
@@ -23,28 +23,28 @@ function generatePassword() {
   var conNum = window.confirm("Press OK to confirm including numeric characters");
   var conUpper = window.confirm("Press OK to confirm including uppercase letters");
   var conLower = window.confirm("Press OK to confirm including lowercase letters");
-
+//if the user doesn't select any characters, the app will end.
   if (!conSymb && !conNum && !conUpper && !conLower) {
     window.alert("You must select at least one character type");
     return;
   }
-
+//creates empty variables
   var temp = '';
   var newPW = '';
-
+//if the user confirmed the use of symbols, temp will include all symbols
   if (conSymb) { temp += symbols };
+  //if the user confirmed numbers, temp will include all numbers + the previous confirmation
   if (conNum) { temp += numbers };
+   //if the user confirmed uppercase letters, temp will include all numbers + the previous confirmation
   if (conUpper) { temp += upperCase };
+   //if the user confirmed lowercase letters, temp will include all numbers + the previous confirmation
   if (conLower) { temp += lowerCase };
 
-
+//temp = all the types of characters selected, and for the number of characters chosen, it takes a random one each time the loop is allowed to go for, and adds it to the var newPW.
   for (var i = 0; i < numChar; i++) {
     newPW += temp[Math.floor(Math.random() * temp.length)];
   }
   return newPW;
-  // var index = Math.floor(Math.random() * options.length);
-  // var computerChoice = options[index];
-  // Check if questions are true,  add all valid strings together, randomize them, .join (" ")
 }
 
 
